11e495d008337b59d39288f4b0b2a8db6f066913
김시영 <siyeong0926@naver.com>
2024-03-13 16:08:49 +0900

    0313 api 연결 성공

--- src/search/lol.service.ts
+++ src/search/lol.service.ts
@@ -9,7 +9,6 @@ import { SummonerDto } from './dto/summoner.dto';
 export class LolService {
   private apiKey: string;
   private baseUrl: string;
-  private requestHeader: { [key: string]: string };
 
   constructor(
     private httpService: HttpService,
@@ -20,105 +19,120 @@ export class LolService {
 
     // Riot Games API 키를 환경변수에서 가져오기
     this.apiKey = this.configService.get<string>('RIOT_API_KEY');
-
-    this.requestHeader = {
-      'User-Agent': this.configService.get<string>('USER_AGENT'),
-      'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
-      'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
-      Origin: this.configService.get<string>('ORIGIN'),
-      'X-Riot-Token': this.configService.get<string>('RIOT_API_KEY'),
-    };
   }
 
   // 소환사 이름으로 소환사 정보 조회
   async findSummonerByName(summonerName: string): Promise<SummonerDto> {
-    const url = `${
-      this.baseUrl
-    }/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}`;
+    const region = 'kr';
+    const url = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(
+      summonerName,
+    )}`;
+    const headers = {
+      'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+      'User-Agent': this.configService.get<string>('USER_AGENT'),
+      'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+      'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+      Origin: this.configService.get<string>('ORIGIN'),
+    };
 
     try {
-      const response = await lastValueFrom(
-        this.httpService.get<SummonerDto>(url, {
-          headers: {
-            'User-Agent': this.requestHeader['User-Agent'],
-            'Accept-Language': this.requestHeader['Accept-Language'],
-            'Accept-Charset': this.requestHeader['Accept-Charset'],
-            Origin: this.requestHeader['Origin'],
-            'X-Riot-Token': this.requestHeader['X-Riot-Token'],
-          },
-        }),
-      );
-      console.log('소환사 이름으로 소환사 정보 조회 ', response.data);
-
-      return response.data;
+      const response = this.httpService.get(url, { headers });
+      const data = await lastValueFrom(response);
+      console.log('findSummonerByName 데이터 조회', data.data);
+      return data.data;
     } catch (error) {
-      console.error(
-        `소환사 데이터를 가져오는 중 오류 발생 , 소환사 이름 : ${summonerName}:`,
-        error.response?.data || error.message,
-      );
-      throw new Error('소환사 데이터를 가져오는 데 실패 했습니다.');
+      console.error('API 요청 중 에러 발생:', error);
+      throw error;
     }
   }
 
   // 소환사 ID를 통해 해당 소환사의 챔피언 숙련도 정보를 가져옴
-  async getChampionMastery(summonerId: string) {
-    const url = `${this.baseUrl}/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerId}`;
-    const response = await lastValueFrom(
-      this.httpService.get(url, {
-        headers: {
-          'User-Agent': this.requestHeader['User-Agent'],
-          'Accept-Language': this.requestHeader['Accept-Language'],
-          'Accept-Charset': this.requestHeader['Accept-Charset'],
-          Origin: this.requestHeader['Origin'],
-          'X-Riot-Token': this.requestHeader['X-Riot-Token'],
-        },
-      }),
-    );
-    console.log('소환사 id 를 통해 챔피언 숙련도 정보 데이터', response.data);
-    return response.data;
+  async getChampionMastery(summonerPuuid: string) {
+    // console.log(
+    //   'getChampionMastery 에서 소환사 PUUID 출력 확인:',
+    //   summonerPuuid,
+    // );
+    const url = `${
+      this.baseUrl
+    }/lol/champion-mastery/v4/champion-masteries/by-puuid/${encodeURIComponent(
+      summonerPuuid,
+    )}`;
+
+    const headers = {
+      'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+      'User-Agent': this.configService.get<string>('USER_AGENT'),
+      'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+      'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+      Origin: this.configService.get<string>('ORIGIN'),
+    };
+
+    try {
+      const response = this.httpService.get(url, { headers });
+      const data = await lastValueFrom(response);
+      console.log('getChampionMastery 데이터 조회', data.data);
+      return data.data;
+    } catch (error) {
+      console.error('getChampionMastery 요청 중 에러 발생:', error);
+      throw error;
+    }
   }
 
-  //최근 경기 가져오기
-  async getRecentMatches(accountId: string) {
-    const url = `${this.baseUrl}/lol/match/v4/matchlists/by-account/${accountId}?endIndex=10`;
-    const response = await lastValueFrom(
-      this.httpService.get(url, {
-        headers: {
-          'User-Agent': this.requestHeader['User-Agent'],
-          'Accept-Language': this.requestHeader['Accept-Language'],
-          'Accept-Charset': this.requestHeader['Accept-Charset'],
-          Origin: this.requestHeader['Origin'],
-          'X-Riot-Token': this.requestHeader['X-Riot-Token'],
-        },
-      }),
-    );
-    console.log('최근 경기 데이터 ', response.data);
-    return response.data;
+  private createHeaders() {
+    return {
+      'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+      'User-Agent': this.configService.get<string>('USER_AGENT'),
+      'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+      'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+      Origin: this.configService.get<string>('ORIGIN'),
+    };
   }
+
+  // 최근 경기 가져오기
+  // async getRecentMatches(accountId: string) {
+  //   const url = `${this.baseUrl}/lol/match/v4/matchlists/by-account/${accountId}?endIndex=10`;
+  //   const headers = {
+  //     'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+  //     'User-Agent': this.configService.get<string>('USER_AGENT'),
+  //     'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+  //     'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+  //     Origin: this.configService.get<string>('ORIGIN'),
+  //   };
+  //   const response = this.httpService.get(url, { headers });
+  //   const data = await lastValueFrom(response);
+  //   console.log('getRecentMatches 데이터 조회', data.data);
+  //   return data.data;
+  // }
 
   //챔피언 목록 ?
-  async getChampions() {
-    const url = `http://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/champion.json`;
-    const response = await lastValueFrom(this.httpService.get(url));
-    console.log('챔피언 목록 가져오기 ', response.data);
-    return response.data;
-  }
+  // async getChampions() {
+  //   const url = `http://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/champion.json`;
+  //   const headers = {
+  //     'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+  //     'User-Agent': this.configService.get<string>('USER_AGENT'),
+  //     'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+  //     'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+  //     Origin: this.configService.get<string>('ORIGIN'),
+  //   };
+  //
+  //   const response = this.httpService.get(url, { headers });
+  //   const data = await lastValueFrom(response);
+  //   console.log('getChampions 데이터 조회', data.data);
+  //   return data.data;
+  // }
 
   //소환사 티어
-  async getSummonerTier(summonerId: string) {
-    const url = `${this.baseUrl}/lol/league/v4/entries/by-summoner/${summonerId}`;
-    const response = await lastValueFrom(
-      this.httpService.get(url, {
-        headers: {
-          'User-Agent': this.requestHeader['User-Agent'],
-          'Accept-Language': this.requestHeader['Accept-Language'],
-          'Accept-Charset': this.requestHeader['Accept-Charset'],
-          Origin: this.requestHeader['Origin'],
-          'X-Riot-Token': this.requestHeader['X-Riot-Token'],
-        },
-      }),
-    );
-    console.log('소환사 티어 데이터 ', response.data);
-    return response.data;
-  }
+  // async getSummonerTier(summonerId: string) {
+  //   const url = `${this.baseUrl}/lol/league/v4/entries/by-summoner/${summonerId}`;
+  //   const headers = {
+  //     'X-Riot-Token': this.configService.get<string>('X-Riot-Token'),
+  //     'User-Agent': this.configService.get<string>('USER_AGENT'),
+  //     'Accept-Language': this.configService.get<string>('ACCEPT_LANGUAGE'),
+  //     'Accept-Charset': this.configService.get<string>('ACCEPT_CHARSET'),
+  //     Origin: this.configService.get<string>('ORIGIN'),
+  //   };
+  //   const response = this.httpService.get(url, { headers });
+  //   const data = await lastValueFrom(response);
+  //   console.log('getSummonerTier 데이터 조회', data.data);
+  //   return data.data;
+  // }
 }
